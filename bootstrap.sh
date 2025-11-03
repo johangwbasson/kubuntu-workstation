5d784e2e84cc28f3b74b0051dfa09b373d453784
Johan Basson <johan.basson@mnscorp.net>
2025-11-03 14:40:07 +0200

    Add check for vars.yml file

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,20 @@
 
 set -eu
 
+# Check for .vars.yml file
+if [ ! -f .vars.yml ]; then
+    echo "=== .vars.yml file not found!"
+    if [ -f .vars-example.yml ]; then
+        cp .vars-example.yml .vars.yml
+        echo "=== Created .vars.yml from .vars-example.yml"
+        echo "=== Please edit .vars.yml with your configuration and run this script again."
+        exit 1
+    else
+        echo "=== ERROR: .vars-example.yml template file not found!"
+        exit 1
+    fi
+fi
+
 echo "=== setup.sh must be run as root."
 
 echo "=== Updating packages and installing git & ansible"
